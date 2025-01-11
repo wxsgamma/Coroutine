35d19eed536ede1e676af1ec2f61db5159d440fb
look-at-gamma <1170834390@qq.com>
2025-01-11 23:02:33 +0800

    1.11.1

---
+++ fiber/fiber.hpp
@@ -0,0 +1,34 @@
+/*
+ * @Date: 2025-01-08 21:17:43
+ * @LastEditTime: 2025-01-11 23:02:10
+ * @FilePath: \Coroutine\fiber\fiber.hpp
+ */
+#ifndef FIBER_HPP
+#define FIBER_HPP
+
+#include <iostream>     
+#include <memory>       
+#include <atomic>       
+#include <functional>   
+#include <cassert>      
+#include <ucontext.h>   
+#include <unistd.h>
+#include <mutex>
+
+namespace Zeus{
+
+class Fiber:public std::enable_shared_from_this<Fiber>{
+
+
+
+    
+}
+
+
+
+
+
+}
+
+
+#endif
